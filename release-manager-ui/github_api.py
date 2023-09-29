9c23495af0c9308fb8cb4a2d5388833bdf3276b9
Petra Číhalová <pcihalov@redhat.com>
2023-09-29 20:37:58 +0200

    refactor: improved open prs download from github

--- release-manager-ui/github_api.py
+++ release-manager-ui/github_api.py
@@ -1,21 +1,43 @@
 import requests
-from flask import current_app
+import re
+from flask import current_app, render_template
 
-from utils import load_json_from_file, save_json_to_file
+from utils import load_json_from_file, save_json_to_file, file_exists
+from config import SERVICES_LINKS, PULL_REQUEST_LIST
+
+
+def get_github_repos_links(json_data):
+    pattern = r'^(https?://)?(www\.)?github\.com/[\w-]+/[\w-]+/?$'
+    gh_links = set()
+    for category in json_data["categories"]:
+        for repo in category["category_repos"]:
+            for link in repo["links"]:
+                if re.match(pattern, link["link_value"]):
+                    gh_links.add(link["link_value"])
+
+    return sorted(list(gh_links), key=lambda x: x.split("/")[-1])
 
 
 def get_open_pull_requests():
-    data = load_json_from_file("repos.json")
-    if not data:
-        return None
+    if not file_exists(SERVICES_LINKS):
+        current_app.logger.warning(f"File '{SERVICES_LINKS}' doesn't exist or doesn't contain links to GitHub/GitLab repos.")
+        error_msg = "The list of repos not found. No data to display."
+        return render_template("errors/404.html", error_msg=error_msg)
+   
+    json_data = load_json_from_file(SERVICES_LINKS)
+    if not json_data:
+        return render_template("errors/error.html", error_msg="")
 
-    github_data = data["github_repos"]
+    github_links = get_github_repos_links(json_data)
 
     pull_requests = {}
+    pattern = r'(?:https?://)?(?:www\.)?github\.com/([\w-]+)/([\w-]+)/?'
 
-    for repo in github_data:
-        owner = repo["repo_link"].split("/")[-2]
-        repo_name = repo["repo_link"].split("/")[-1]
+    for url in github_links:
+        match = re.search(pattern, url)
+        if match:
+            owner = match.group(1)
+            repo_name = match.group(2)
 
         gh_token = current_app.config["GITHUB_TOKEN"]
 
@@ -35,4 +57,4 @@ def get_open_pull_requests():
         else:
             pull_requests[repo_name] = None
 
-    save_json_to_file(pull_requests, "pull_requests.json")
+    save_json_to_file(pull_requests, PULL_REQUEST_LIST)
