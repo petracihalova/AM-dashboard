c56e3c4766ce6b84b89a573de7dac74d23e6b73a
Petra Číhalová <pcihalov@redhat.com>
2023-09-29 20:02:29 +0200

    refactor: filenames saved into variables

--- release-manager-ui/config.py
+++ release-manager-ui/config.py
@@ -1,7 +1,13 @@
 import os
+from pathlib import Path
 
 
 BACKEND_API = os.environ.get("BACKEND_API")
 
 GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
 GITLAB_TOKEN = os.environ.get("GITLAB_TOKEN")
+
+# Filenames
+SERVICES_LINKS = Path("release-manager-ui/data/services_links.json")
+SERVICES_LINKS_EXAMPLE = Path("release-manager-ui/data/services_links_example.json")
+PULL_REQUEST_LIST = Path("release-manager-ui/data/pull_request_list.json")
