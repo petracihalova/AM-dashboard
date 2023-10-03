80d2e22d451c0bacc5dfa17fc3f21806cbde4075
Petra Číhalová <pcihalov@redhat.com>
2023-10-03 08:15:53 +0200

    release notes improvement - added release date

--- release-manager-ui/static/js/release_notes.js
+++ release-manager-ui/static/js/release_notes.js
@@ -12,10 +12,27 @@ document.addEventListener('DOMContentLoaded', function () {
       // Write the ClipboardItem to the clipboard
       await navigator.clipboard.write([clipboardItem]);
 
-      // Provide visual feedback or alert
-      alert('Content copied to clipboard!');
     } catch (err) {
       console.error('Failed to copy content: ', err);
     }
   });
+
+  // Function to format a date as "yyyy-mm-dd"
+  function formatDate(date) {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Add leading zero if needed
+    const day = String(date.getDate()).padStart(2, '0'); // Add leading zero if needed
+    return `${year}-${month}-${day}`;
+  }
+
+  // Get the current date
+  const currentDate = new Date();
+
+  // Format the date
+  const formattedDate = formatDate(currentDate);
+
+  // Insert the formatted date into the H2 element
+  const dateDisplayElement = document.getElementById("dateDisplay");
+  dateDisplayElement.textContent = formattedDate;
+
 });
